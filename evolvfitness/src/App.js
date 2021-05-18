eb46cb4ab2a39e7348012542b27295b094480638
Prashant Soni <30834438+cjindal-innow8@users.noreply.github.com>
2021-05-18 23:57:51 +0530

    Initiate with setup

---
+++ evolvfitness/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import AdminPanel from './components/adminComponents/index'
+
+function App() {
+  return (
+    <div className="App">
+    <div>
+      Evolv Fitness Challenge
+    </div>
+     <AdminPanel/>
+    </div>
+  );
+}
+
+export default App;
---
+++ evolvfitness/src/components/adminComponents/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AdminPanel() {
+    return (
+        <div>
+            This is my admin panel
+        </div>
+    )
+}
+
+export default AdminPanel;
